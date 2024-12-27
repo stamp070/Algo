5c32413ac93ef1c0944c5ebed4860a8fa3044c27
stamp070 <101258175+stamp070@users.noreply.github.com>
2024-12-27 12:04:22 +0700

    optimize

--- midterm/2_2.cpp
+++ midterm/2_2.cpp
@@ -1,29 +1,31 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-int main()
-{
-    int n, find;
-    cin >> n >> find;
-    int A[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> A[i];
-    }
-    int min = 1000, temp;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (A[i] + A[j] == find)
-            {
-                temp = abs(A[i] - A[j]);
-            }
-            if (temp < min)
-            {
-                min = temp;
-            }
-        }
-    }
-    cout << min << endl;
+int main(){
+	int n,find;
+	cin >> n >> find;
+	vector<int> arry(n);
+	
+	for(int i=0;i<n;i++){
+		cin >> arry[i];
+	}
+	sort(arry.begin(),arry.end());
+	int diff = 10000;
+	int left = 0, right = n-1;
+	while(left<=right){
+		int sum = arry[left]+arry[right];
+		if(sum == find){
+			if(abs(arry[left]-arry[right]) < diff){
+				diff = abs(arry[left]-arry[right]);
+				left++;
+				right--;
+			}
+		}else if(sum < find){
+			left++;
+		}else{
+			right--;
+		}
+	}
+	cout << diff;
 }

f3805bbe1dcfd34bedbaf809be86df8adfa5a83d
stamp070 <101258175+stamp070@users.noreply.github.com>
2025-01-05 14:16:42 +0700

    Update

--- midterm/union_intersect.cpp
+++ midterm/union_intersect.cpp
@@ -1,54 +1,64 @@
 #include <iostream>
-#include <algorithm>
-#include <unordered_set>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
+void Union(int a[], int b[], int n)
+{
+    int Max = max(*max_element(a, a + n), *max_element(b, b + n));
+    vector<int> Union(Max + 1, 0);
+    for (int i = 0; i < n; i++)
+    {
+        if (Union[a[i]] == 0)
+        {
+            cout << a[i] << " ";
+            Union[a[i]]++;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (Union[b[i]] == 0)
+        {
+            cout << b[i] << " ";
+            Union[b[i]]++;
+        }
+    }
+    cout << endl;
+}
+void intersection(int a[], int b[], int n)
+{
+    int Max = max(*max_element(a, a + n), *max_element(b, b + n));
+    vector<int> Intersect(Max + 1, 0);
+    for (int i = 0; i < n; i++)
+    {
+        Intersect[b[i]]++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (Intersect[a[i]] > 0)
+        {
+            cout << a[i] << " ";
+            Intersect[a[i]] = 0;
+        }
+    }
+    cout << endl;
+}
 int main()
 {
     int n;
     cin >> n;
     int a[n], b[n];
-    unordered_set<int> union_temp;
-    unordered_set<int> intersect_temp;
-    vector<int> union_ans;
-    vector<int> intersect_ans;
+
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
-        if (union_temp.find(a[i]) == union_temp.end())
-        {
-            union_temp.insert(a[i]);     // union insert แบบข้างหน้า
-            union_ans.push_back(a[i]);   // เลยใช้ vector ที่ insert แบบข้างหลังช่วย
-            intersect_temp.insert(a[i]); // เอา a[i] ไปเก็บก่อนแล้วค่อยเช็คว่า
-        }
     }
     for (int i = 0; i < n; i++)
     {
         cin >> b[i];
-        if (union_temp.find(b[i]) == union_temp.end())
-        {
-            union_temp.insert(b[i]);
-            union_ans.push_back(b[i]);
-        }
     }
-    for (int i = 0; i < n; i++)
-    {
-        if (intersect_temp.find(b[i]) != intersect_temp.end())
-        {
-            intersect_ans.push_back(b[i]);
-        }
-    }
-    for (auto i : intersect_ans)
-    {
-        cout << i << " ";
-    }
-    cout << endl;
 
-    for (auto i : union_ans)
-    {
-        cout << i << " ";
-    }
+    intersection(a, b, n);
+    Union(a, b, n);
 
     return 0;
 }

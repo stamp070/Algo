cec12515b772d0ec05afc9d0eab03a70c3e5a7f3
stamp070 <101258175+stamp070@users.noreply.github.com>
2025-03-06 16:32:39 +0700

    Create 1.cpp

---
+++ final/week5/1.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+int prim(vector<pair<int, int>> g[], int src, int n)
+{
+    vector<bool> visited(n + 1, false);
+    vector<int> dist(n + 1, 10000);
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    //    weight,src
+    pq.push({0, src});
+    visited[src] = true;
+    dist[src] = 0;
+
+    int sum = 0;
+    while (!pq.empty())
+    {
+        int temp = pq.top().second;
+
+        pq.pop();
+        for (auto i : g[temp])
+        {
+            int v = i.first;
+            int weight = i.second;
+            if (!visited[v] && weight < dist[v])
+            {
+                cout << "node: " << v << " weight: " << weight << endl;
+                visited[v] = true;
+                dist[v] = weight;
+                pq.push({dist[v], v});
+                sum += dist[v];
+            }
+        }
+    }
+    return sum;
+}
+int main()
+{
+    int v, e;
+    cin >> v >> e;
+    vector<pair<int, int>> g[v + 1];
+
+    for (int i = 0; i < e; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        g[a].push_back({b, c});
+        g[b].push_back({a, c});
+    }
+    int dest;
+    cin >> dest;
+    cout << prim(g, dest, v);
+    return 0;
+}
